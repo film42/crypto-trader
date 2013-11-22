648c3e956c0d91e796b324806f98d3515fc937c4
Garrett Thornburg <film42@gmail.com>
2013-11-22 15:16:37 -0700

    login page good enough; working; wtf assets tho

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,39 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
+
+//= require ../others/plugins/jquery-1.8.3.min.js
+//= require ../others/plugins/jquery-ui/jquery-ui-1.10.1.custom.min.js
+//= require ../others/plugins/bootstrap/js/bootstrap.min.js
+//= require ../others/plugins/breakpoints.js
+//= require ../others/plugins/jquery-unveil/jquery.unveil.min.js
+
+
+//= require ../others/plugins/pace/pace.min.js
+//= require ../others/plugins/data-tables/jquery.dataTables.js
+
+//= require ../others/plugins/jquery-validation/js/jquery.validate.min.js
+//= require ../others/plugins/Chartjs/Chart.min.js
+
+//= require ../others/plugins/jquery-slimscroll/jquery.slimscroll.min.js
+//= require ../others/plugins/jquery-numberAnimate/jquery.animateNumbers.js
+//= require ../others/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js
+//= require ../others/plugins/jquery-block-ui/jqueryblockui.js
+//= require ../others/plugins/bootstrap-select2/select2.min.js
+//= require ../others/plugins/jquery-ricksaw-chart/js/raphael-min.js
+//= require ../others/plugins/jquery-ricksaw-chart/js/d3.v2.js
+//= require ../others/plugins/jquery-ricksaw-chart/js/rickshaw.min.js
+//= require ../others/plugins/jquery-morris-chart/js/morris.min.js
+//= require ../others/plugins/jquery-easy-pie-chart/js/jquery.easypiechart.min.js
+//= require ../others/plugins/jquery-slider/jquery.sidr.min.js
+//= require ../others/plugins/jquery-jvectormap/js/jquery-jvectormap-1.2.2.min.js
+//= require ../others/plugins/jquery-jvectormap/js/jquery-jvectormap-us-lcc-en.js
+//= require ../others/plugins/jquery-sparkline/jquery-sparkline.js
+//= require ../others/plugins/jquery-flot/jquery.flot.min.js
+//= require ../others/plugins/jquery-flot/jquery.flot.animator.min.js
+//= require ../others/plugins/skycons/skycons.js
+
+//= require arch/dashboard.js
+//= require arch/core.js
+//= require arch/demo.js
+//= require arch/login.js
--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,7 @@ module CryptoTrader
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.paths << Rails.root.join('app', 'assets', 'others')
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
