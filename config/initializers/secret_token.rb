aba60d1128b7600a992390407d6335f2917abc28
Garrett Thornburg <film42@gmail.com>
2013-11-22 00:56:25 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CryptoTrader::Application.config.secret_token = '42dfa1f6f7d17e199d024acc794637a284a3065f0b41f9df14a53acdf38a77ca4693d0124295218131647b1f3172106bdff04fc9779af55fcfbd00eb13a9d10d'
